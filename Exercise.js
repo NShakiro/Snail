f09a659212234bfa1e4a0da59a04099f9e9bdbaa
NShakiro <50278955+NShakiro@users.noreply.github.com>
2021-02-23 12:30:19 -0700

    Update Exercise.js

--- Exercise.js
+++ Exercise.js
@@ -1,3 +1,7 @@
+//The snail climbs up 7 feet each day and slips back 2 feet each night.
+//How many days will it take the snail to get out of a well with the given depth?
+
+
 function main() {
     var depth = parseInt(readLine(), 10);
     var day = 0;
